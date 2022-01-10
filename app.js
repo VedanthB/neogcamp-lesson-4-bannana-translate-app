440316b2f0cec006d9233d0a7b7066bf513ea059
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2022-01-11 03:30:26 +0530

    Updated functions to ES6 arrow functions 🎃

--- app.js
+++ app.js
@@ -6,17 +6,17 @@ var outputDiv = document.querySelector("#output");
 var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
 
-function getTranslationURL(input) {
+const getTranslationURL = input => {
     return serverURL + "?" + "text=" + input
 }
-
-function errorHandler(error) {
+ 
+const errorHandler = error => {
     console.log("error occured", error);
     alert("something wrong with server! try again after some time")
 }
 
 
-function clickHandler() {
+const clickHandler = () => {
     var inputText = txtInput.value; // taking input
 
     // calling server for processing
